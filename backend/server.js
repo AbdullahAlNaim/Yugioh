2997d40f6095cbf1a8f34b47a036fcfa74495ce4
AbdullahAlNaim <aanaim96@gmail.com>
2024-09-09 00:15:29 -0400

    switch frontend using own backend api

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.port || 3000;
 const cardRoutes = require('./src/routes/cardRoutes')
 
 app.use(express.json());
 // app.use(router);
-// app.use(cors());
+app.use(cors());
 
 app.use('/api', cardRoutes);
 
